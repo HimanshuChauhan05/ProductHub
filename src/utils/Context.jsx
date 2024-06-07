ea390f9c0033ca5041b25a0084afacd2c63e53e2
HimanshuChauhan05 <chauhanhimanshu260@gmail.com>
2024-06-07 16:43:34 +0530

    Second commit

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -1,7 +1,19 @@
+import axios from "./axios";
 import React, { createContext, useEffect, useState } from "react";
 
-
 export const ProductContext = createContext();
+const getProducts = async()=>{
+    try {
+        const { data } = await axios("/products/")
+        setProducts(data)
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+useEffect(()=>{
+    getProducts();
+}, []);
 
 const Context = (props) => {
 
